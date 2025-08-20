145237dd553ab9558b87c831234a21c63877c1ed
eankhbayar <eankhbayarr@gmail.com>
2025-08-20 11:09:48 +0800

    chap3 part0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+
+    run(address)?.await
 }
